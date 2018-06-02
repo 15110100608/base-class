88bacdedcf7c087edc782476f877ede3e93e0269
杨烨 <yywxlyy@qq.com>
2018-06-02 11:15:19 +0800

    v0.1.4

--- baesclass/src/main/java/com/xiaoye/baseclass/base/BaseAdapter.java
+++ baesclass/src/main/java/com/xiaoye/baseclass/base/BaseAdapter.java
@@ -7,6 +7,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.xiaoye.baseclass.utils.DensityUtil;
 
 import java.util.List;
 
@@ -74,5 +77,33 @@ public abstract class BaseAdapter<T> extends RecyclerView.Adapter {
     @Override
     public int getItemViewType(int position) {
         return position;
+    }
+
+    /*
+     * 短时间Toast的简化
+     */
+    public void showToastShort(String text){
+        Toast.makeText(context,text,Toast.LENGTH_SHORT).show();
+    }
+
+    /*
+     * 长时间Toast的简化
+     */
+    public void showToastLong(String text){
+        Toast.makeText(context,text,Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    public float dip2px(float dpValue){
+        return DensityUtil.dip2px(context,dpValue);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public float px2dip(float pxValue) {
+        return DensityUtil.px2dip(context,pxValue);
     }
 }
